0501de1cd7a806b02fcb2f88abe7c80efeb31727
Shadrack Mballah <129520133+shadrackjm@users.noreply.github.com>
2024-04-25 18:14:01 +0300

    Update SendSMSController.php

--- app/Http/Controllers/SendSMSController.php
+++ app/Http/Controllers/SendSMSController.php
@@ -20,8 +20,8 @@ class SendSMSController extends Controller
 
     public function sendSMS(Request $request){
         $configuration = new Configuration(
-            host: 'k28z4e.api.infobip.com', //get these data from the account dashboard
-            apiKey: '99de93bd0e671ef99355b3bdcc332eea-75607338-5132-46dd-bd3b-c32f3766fb9a' // now replace these data with the actual ones from infobip account..
+            host:'', //get these data from the account dashboard
+            apiKey: '' // now replace these data with the actual ones from infobip account..
         );
         
         $sendSmsApi = new SmsApi(config: $configuration);
